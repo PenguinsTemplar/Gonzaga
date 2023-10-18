4e3bc787d4f4136ae0b0232fcf0516026eebf020
Tyler <penguincavalry@gmail.com>
2023-10-18 14:48:52 -0600

    getting some more test stuff up

---
+++ demo.js
@@ -0,0 +1,7 @@
+//JavaScript
+const ostrichBtn = document.getElementById("ostrichBtn");
+
+        ostrichBtn.addEventListener("click", function(e) {
+            console.log("Button was clicky")
+            ostrichBtn.style.backgroundColor = "red";
+        });
